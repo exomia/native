2c59e7724ebde6abf56f6ba6c3a401d3be3bc201
Daniel Baetz <baetz.daniel@gmail.com>
2019-06-21 00:07:25 +0200

    license changed

--- Exomia.Native/Allocator/ByteArrayPool2Allocator.cs
+++ Exomia.Native/Allocator/ByteArrayPool2Allocator.cs
@@ -1,24 +1,10 @@
-﻿#region MIT License
+﻿#region License
 
-// Copyright (c) 2019 exomia - Daniel Bätz
+// Copyright (c) 2018-2019, exomia
+// All rights reserved.
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
 
 #endregion
 
@@ -26,12 +12,12 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// ReSharper disable ArrangeRedundantParentheses
+
 namespace Exomia.Native.Allocator
 {
-    // ReSharper disable ArrangeRedundantParentheses
-
     /// <summary>
-    ///     UnsafeByteArrayAllocator2 class.
+    ///     A byte array pool 2 allocator. This class cannot be inherited.
     /// </summary>
     public sealed unsafe class ByteArrayPool2Allocator : IDisposable
     {
@@ -157,7 +143,8 @@ namespace Exomia.Native.Allocator
                         if (_bucketCount[bucketIndex] > 0)
                         {
                             *(ptr - 1) = _bucketHead[bucketIndex]; // set next on current head index
-                            _bucketHead[bucketIndex] = *(ptr - 2); // set the head now on this elements index
+                            _bucketHead[bucketIndex] =
+                                *(ptr - 2); // set the head now on this elements index
                             _bucketCount[bucketIndex]--;
                         }
                     }
--- Exomia.Native/Collections/List.cs
+++ Exomia.Native/Collections/List.cs
@@ -1,24 +1,10 @@
-﻿#region MIT License
+﻿#region License
 
-// Copyright (c) 2019 exomia - Daniel Bätz
+// Copyright (c) 2018-2019, exomia
+// All rights reserved.
 // 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
 
 #endregion
 
@@ -32,7 +18,7 @@ namespace Exomia.Native.Collections
     /// <summary>
     ///     a faster list for unmanaged types.
     /// </summary>
-    /// <typeparam name="T"> unmanaged. </typeparam>
+    /// <typeparam name="T"> Generic type parameter. </typeparam>
     public unsafe class List2<T> where T : unmanaged
     {
         /// <summary>
@@ -68,10 +54,8 @@ namespace Exomia.Native.Collections
         /// <summary>
         ///     Sets the capacity.
         /// </summary>
-        /// <exception cref="ArgumentOutOfRangeException">
-        ///     Thrown when one or more arguments are outside
-        ///     the required range.
-        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown when one or more arguments are outside
+        ///                                                the required range. </exception>
         /// <value>
         ///     The capacity.
         /// </value>
@@ -151,10 +135,8 @@ namespace Exomia.Native.Collections
         ///     Initializes a new instance of the &lt;see cref="List2&lt;T&gt;"/&gt; class.
         /// </summary>
         /// <param name="capacity"> The capacity. </param>
-        /// <exception cref="ArgumentOutOfRangeException">
-        ///     Thrown when one or more arguments are outside
-        ///     the required range.
-        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown when one or more arguments are outside
+        ///                                                the required range. </exception>
         public List2(int capacity)
         {
             if (capacity < 0) { throw new ArgumentOutOfRangeException(nameof(capacity)); }
